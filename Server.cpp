b41dd92f4e604ed2ca7006b95272754194316d69
hamzah1432 <halmuhesn@gmail.com>
2026-06-16 08:47:12 +0300

    Day 2

--- Server.cpp
+++ Server.cpp
@@ -6,7 +6,7 @@
 /*   By: halmuhis <halmuhesn@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/14 16:44:57 by halmuhis          #+#    #+#             */
-/*   Updated: 2026/06/15 02:04:21 by halmuhis         ###   ########.fr       */
+/*   Updated: 2026/06/16 08:23:56 by halmuhis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,61 @@
 #include <arpa/inet.h>
 #include <iostream>
 #include <unistd.h>
+#include <fcntl.h>
+#include <cstring>
+
+// =============================================================================
+// 1. Orthodox Canonical Form
+// =============================================================================
 
 Server::Server(int port) : _listen_fd(-1), _port(port) {}
-Server::~Server() {}
+Server::~Server()
+{
+	if (_listen_fd >= 0)
+		close(_listen_fd);
+}
+
+// =============================================================================
+// 2. Helper Function
+// =============================================================================
+
+void Server::setNonBlocking(int fd)
+{
+	if (fcntl(fd, F_SETFL, O_NONBLOCK) < 0)
+		throw std::runtime_error("Failed to set non-blocking");
+}
+
+void Server::acceptNewClient()
+{
+	int client_fd = accept(_listen_fd, NULL, NULL);
+	if (client_fd < 0)
+		return;
+	setNonBlocking(client_fd);
+	struct pollfd client_pfd;
+	client_pfd.fd = client_fd;
+	client_pfd.events = POLLIN;
+	client_pfd.revents = 0;
+	_poll_fds.push_back(client_pfd);
+
+	Client new_client;
+	new_client.fd = client_fd;
+	new_client.read_buffer = "";
+	new_client.write_buffer = "";
+	_clients[client_fd] = new_client;
+}
+
+void Server::closeClient(size_t i)
+{
+	int fd = _poll_fds[i].fd;
+
+	close(fd);
+	_clients.erase(fd);
+	_poll_fds.erase(_poll_fds.begin() + i);
+}
+
+// =============================================================================
+// 3. Main Functions
+// =============================================================================
 
 void Server::setup()
 {
@@ -51,29 +103,102 @@ void Server::setup()
 	{
 		throw std::runtime_error("Failed to listen on socket");
 	}
+
+	setNonBlocking(_listen_fd);
+
+	struct pollfd listen_pfd;
+	listen_pfd.fd = _listen_fd;
+	listen_pfd.events = POLLIN;
+	listen_pfd.revents = 0;
+	_poll_fds.push_back(listen_pfd);
 }
 
 void Server::run()
 {
 	while (true)
 	{
-		struct sockaddr_in client_addr;
-		socklen_t client_len = sizeof(client_addr);
-		int client_fd = accept(_listen_fd, (struct sockaddr *)&client_addr, &client_len);
-		if (client_fd < 0)
+		int ret = poll(&_poll_fds[0], _poll_fds.size(), -1);
+
+		if (ret < 0)
 			continue;
 
-		std::cout << "Client connected!" << std::endl;
-
-		char buffer[4096];
-		ssize_t bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-		if (bytes_received > 0)
+		for (size_t i = 0; i < _poll_fds.size(); i++)
 		{
-			buffer[bytes_received] = '\0';
-			std::cout << "Received: " << buffer << std::endl;
-			const char *response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
-			send(client_fd, response, std::strlen(response), 0);
+			if (_poll_fds[i].revents == 0)
+				continue;
+
+			if (_poll_fds[i].fd == _listen_fd)
+			{
+				if (_poll_fds[i].revents & POLLIN)
+				{
+					acceptNewClient();
+				}
+			}
+			else
+			{
+				int fd = _poll_fds[i].fd;
+				Client &client = _clients[fd];
+
+				if (_poll_fds[i].revents & (POLLERR | POLLHUP | POLLNVAL))
+				{
+					closeClient(i);
+					i--;
+					continue;
+				}
+
+				if (_poll_fds[i].revents & POLLIN)
+				{
+					char buf[4096];
+					ssize_t n = recv(fd, buf, sizeof(buf), 0);
+
+					if (n <= 0)
+					{
+						closeClient(i);
+						i--;
+						continue;
+					}
+					else
+					{
+						client.read_buffer.append(buf, n);
+
+						if (client.read_buffer.find("\r\n\r\n") != std::string::npos)
+						{
+							std::string response = "HTTP/1.1 200 OK\r\n"
+												   "Content-Type: text/plain\r\n"
+												   "Content-Length: 13\r\n"
+												   "Connection: close\r\n\r\n"
+												   "Hello, world!";
+
+							client.write_buffer = response;
+							_poll_fds[i].events = POLLIN | POLLOUT;
+							client.read_buffer.clear();
+						}
+					}
+				}
+
+				if (_poll_fds[i].revents & POLLOUT)
+				{
+					ssize_t n = send(fd, client.write_buffer.c_str(), client.write_buffer.size(), 0);
+
+					if (n <= 0)
+					{
+						closeClient(i);
+						i--;
+						continue;
+					}
+					else
+					{
+						client.write_buffer.erase(0, n);
+
+						if (client.write_buffer.empty())
+						{
+							closeClient(i);
+							i--;
+							continue;
+						}
+					}
+				}
+			}
 		}
-		close(client_fd);
 	}
 }
--- Server.hpp
+++ Server.hpp
@@ -6,7 +6,7 @@
 /*   By: halmuhis <halmuhesn@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/14 16:36:20 by halmuhis          #+#    #+#             */
-/*   Updated: 2026/06/15 01:46:56 by halmuhis         ###   ########.fr       */
+/*   Updated: 2026/06/16 08:19:53 by halmuhis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,18 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <stdexcept>
+#include <fcntl.h>
+#include <vector>
+#include <map>
+#include <string>
+#include <poll.h>
+
+struct Client
+{
+	int fd;
+	std::string read_buffer;
+	std::string write_buffer;
+};
 
 class Server
 {
@@ -23,14 +35,19 @@ private:
 	int _listen_fd;
 	int _port;
 	struct sockaddr_in _addr;
-
+	std::vector<struct pollfd> _poll_fds;
+	std::map<int, Client> _clients;
+	// Orthodox Canonical Form
 	Server(const Server &other);
 	Server &operator=(const Server &other);
+	// Helper Functions
+	void setNonBlocking(int fd);
+	void acceptNewClient();
+	void closeClient(size_t i);
 
 public:
 	Server(int port);
 	~Server();
-
 	void setup();
 	void run();
 };
